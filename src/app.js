bd6720e551352b10f742a059cf6d77da93a249a6
sandeepkumar <palsandeep894@hotmail.com>
2024-01-25 21:42:47 +0530

    update

--- src/app.js
+++ src/app.js
@@ -16,4 +16,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// import routes
+
+import userRouter from "./router/user.routes.js";
+
+app.use("/api/v1/user", userRouter);
+
 export { app };
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,38 @@
+import { v2 } from "cloudinary";
+import fs from "fs";
+import { loadavg } from "os";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadFileOnCloudinayry = async (localfile) => {
+  if (!localfile) return null;
+
+  //   upload file on cloudinary
+
+  try {
+    const response = await cloudinary.uploader.upload(localfile, {
+      resource_type: "auto",
+    });
+    // file has been uploaded successfully
+    console.log("file has been uploaded", response);
+    return response.url;
+  } catch (error) {
+    fs.unlinkSync(localfile);
+    // remove the locally saved temporary file  as the upload operation got failed
+    return null;
+  }
+};
+
+// cloudinary.uploader.upload(
+//   "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" },
+//   function (error, result) {
+//     console.log(result);
+//   }
+// );
+
+export { uploadFileOnCloudinayry };
