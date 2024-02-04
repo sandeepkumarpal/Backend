e1e2b1d35c14226a94f1bb10317245cfaa54de11
sandeepkumar <palsandeep894@hotmail.com>
2024-02-04 20:59:52 +0530

    images upload

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,50 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.models.js";
+import { uploadFileOnCloudinayry } from "../utils/cloudinary.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "RESPONSE AAA GYA BHAI",
+  const { fullName, email, username, password } = req.body;
+  console.log(email);
+
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "all fields are required");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ email }, { username }],
   });
+  if (existedUser) {
+    throw new ApiError(409, "user with email or username is already exists");
+  }
+
+  console.log(req.files, "files");
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "avatar field is required");
+  }
+  const avatar = await uploadFileOnCloudinayry(avatarLocalPath);
+  const coverImage = await uploadFileOnCloudinayry(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "error in uploading avatar");
+  }
+
+  User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+  // res.status(200).json({
+  //   message: "RESPONSE AAA GYA BHAI",
+  // });
 });
 
 export { registerUser };
