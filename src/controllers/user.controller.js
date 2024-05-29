1f5033d7ad63d164ba50019ffcdc1967b5de8d3c
sandeepkumar <palsandeep894@hotmail.com>
2024-05-29 12:58:06 +0530

    login and logout functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -97,11 +97,63 @@ const loginUser = asyncHandler(async (req, res) => {
   });
   // check user
   if (!user) throw new ApiError(404, "User does not exist");
-  // check access and refresh token
 
+  // check password
   const isPasswordValid = await user.isPasswordCorrect(password);
   if (!isPasswordValid) throw new ApiError(401, "Invalid credentials");
-  // Send cookie
+  // generate access and refresh token
+  const { accessToken, refreshToken } = await generateAccessandRefreshToken(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  // Send cookie and send response
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged in Successfully"
+      )
+    );
 });
 
-export { registerUser, loginUser };
+const logoutUser = asyncHandler(async (req, res) => {
+  User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out succesfully "));
+});
+
+export { registerUser, loginUser, logoutUser };
